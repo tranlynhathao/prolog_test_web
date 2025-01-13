cba9ee2e5f5f69d73699d5bda8a94af60c2b85a8
tranlynhathao <haotran04022005@gmail.com>
2025-01-13 20:57:12 +0700

    create: first commit

---
+++ run.pl
@@ -0,0 +1,6 @@
+% Main file to run the Web Prolog.
+% Loads the demo and starts a server
+
+:- [load].
+
+:- node(localhost:3000).
---
+++ server.pl
@@ -0,0 +1,14 @@
+/** <module> Prolog Web Server
+*/
+:- module(server, [ ]).
+
+:- multifile user:file_search_path/2.
+:- dynamic user:file_search_path/2.
+
+:- prolog_load_context(directory, Dir), asserta(user:file_search_path(app, Dir)).
+
+% user:file_search_path(www, app(www)).
+% user:file_search_path(apps, app(apps)).
+%
+% :- http_handler(root(apps), serve_files_in_directory(apps), [prefix]).
+% :- http_handler(root(.), serve_files_in_directory(www), [prefix]).
