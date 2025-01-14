a71783aa000ec79b786afcbf3b4f54d6fa2ea68d
tranlynhathao <haotran04022005@gmail.com>
2025-01-14 08:54:19 +0700

    create: homepage & favicon

---
+++ lib/admin/admin.pl
@@ -0,0 +1,8 @@
+:- module(admin, []).
+
+:- use_module(library(http/http_dispatch)).
+:- use_module(library(http/http_json)).
+:- use_module(library(http/http_parameters)).
+:- use_module(library(pairs)).
+:- use_module(library(settings)).
+:- use_module(library(pengines)).
--- server.pl
+++ server.pl
@@ -7,8 +7,10 @@
 
 :- prolog_load_context(directory, Dir), asserta(user:file_search_path(app, Dir)).
 
-% user:file_search_path(www, app(www)).
-% user:file_search_path(apps, app(apps)).
-%
-% :- http_handler(root(apps), serve_files_in_directory(apps), [prefix]).
-% :- http_handler(root(.), serve_files_in_directory(www), [prefix]).
+user:file_search_path(www, app(www)).
+user:file_search_path(apps, app(apps)).
+
+:- http_handler(root(apps), serve_files_in_directory(apps), [prefix]).
+:- http_handler(root(.), serve_files_in_directory(www), [prefix]).
+
+:- http_handler(root(tutorial), http_reply_file('www/tutorial.html', []), [prefix]).
